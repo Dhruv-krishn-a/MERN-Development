2755a45e4ad75bd88ada5713e1b4c13fcebec893
Dhruv-krishn-a <Dhruvkrishna1055@gmail.com>
2024-02-19 22:32:51 +0530

    DATATYPES

---
+++ 01-Basics/const.js
@@ -0,0 +1,27 @@
+console.log("Dhruv Krishna")
+const accountEmail = "dhruv@google.com"
+var accountPassword =  "1234456 "
+accountCIty ="Jaipur"
+
+// accountId = 2 //not allowed
+//noconsole.log(accountId);
+
+console.log([accountEmail, accountPassword, accountCIty]);
+
+"use strict"
+console.log("3"+"3");
+let name="Dhruv"
+let age = 18
+let isLoggedin = false
+
+//number= 2^53
+//bigint 
+//string = ""
+//boolean = true/false
+//null = standalone value
+//undefined
+//symbol =unique
+
+//object
+
+console.log(typeof age)
