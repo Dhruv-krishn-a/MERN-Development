d6fe93e184665368388e3bbbb37fa94d6ea9dc7d
Dhruv-krishn-a <Dhruvkrishna1055@gmail.com>
2024-02-20 18:10:26 +0530

    Learned Comparisons b/s diffrent operators

--- 01-Basics/03_conversionOoperations.js
+++ 01-Basics/03_conversionOoperations.js
@@ -15,3 +15,41 @@ let isLoggedin = 1
 let booleanIsLoggedIn = Boolean(isLoggedin)
 console.log(booleanIsLoggedIn);
 //number can also be converted to boolean
+
+// ****************************OPERATIONS*********************************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+
+let str1= "Dhruv"
+let str2 = "krishna"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1+ "2")
+console.log("1" + 2 + 2) // This will give output 122 
+console.log(1 + 2 + "2") // This will give output 32
+//these are all guidelines driven .
+
+console.log(+true)
+console.log(+"")
+
+//let num4,num5,num6
+//num1=num2=num3 = 2+2
+//avoid writting this type of code at any cost 
+let [num1,num2,num3]= Array(3).fill(4)//use this.
+
+//increment and decrement operatords
+let gamecounter =100
+console.log(gamecounter++)
+console.log(gamecounter);
+
+
+
---
+++ 01-Basics/04_comparisons.js
@@ -0,0 +1,22 @@
+//These are basic conversions 
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+
+// This will automaticaly be converted to number 
+console.log("2">1);
+console.log("2">1);
+
+//Problem with conversions with null, avoid them
+console.log(null<0);
+console.log(null==0);
+console.log(null>=0); //This will give output true
+
+// ==
+
+console.log("2"=== 2)
+
+
