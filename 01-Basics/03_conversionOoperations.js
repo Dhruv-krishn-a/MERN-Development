d611a6f8dfa8a8d007c15c75dd80f0e615ba6605
Dhruv-krishn-a <Dhruvkrishna1055@gmail.com>
2024-02-20 17:23:36 +0530

    Learned Conversions

---
+++ 01-Basics/03_conversionOoperations.js
@@ -0,0 +1,18 @@
+let score = "33abc" //This will convert to number
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof score);
+//this is not a numpber -> special type not a number NaN
+console.log(valueInNumber)
+
+// When we convert a number it will easily be converted.
+//number and string will be converted to NaN
+//true-> 1; false -> 0;
+// for null it will give output to 0
+//to convert to boolean 
+let isLoggedin = 1
+let booleanIsLoggedIn = Boolean(isLoggedin)
+console.log(booleanIsLoggedIn);
+
+number can also be converted to boolean
