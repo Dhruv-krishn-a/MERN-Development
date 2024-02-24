c143c382a4226c5756a55fc520d58b3875e3f521
Dhruv-krishn-a <Dhruvkrishna1055@gmail.com>
2024-02-25 02:35:25 +0530

    Understanding Arrays and operations

--- 02-Basics/02_array.js
+++ 02-Basics/02_array.js
@@ -1,22 +1,23 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
+console.log("Pushes the dc_heroes into the marvel Heroes\n ")
+marvel_heros.push(dc_heros)
+console.log("Marvel_heroes array after pushing dc heroes\n ");
+console.log(marvel_heros);
+console.log(marvel_heros[3][1]);
 
-// marvel_heros.push(dc_heros)
-
-// console.log(marvel_heros);
-// console.log(marvel_heros[3][1]);
-
-// const allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);
-
+const allHeros = marvel_heros.concat(dc_heros)
+console.log("Concatination of Marvel_heroes and dc_heroes\n ",allHeros);
+//Push pushes on existing array , concat make new array
+// Spreading array - one one element 
 const all_new_heros = [...marvel_heros, ...dc_heros]
 
-// console.log(all_new_heros);
+console.log("Spread of MArvel_heroes and Dc_heroes\n ",all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+console.log("spreading array within array using flat function \n",real_another_array);
 
 
 
