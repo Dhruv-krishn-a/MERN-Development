bed7b2966dc8ed09d9598bd3a80cc5ff46997700
Dhruv-krishn-a <Dhruvkrishna1055@gmail.com>
2024-03-07 19:05:07 +0530

    functions

---
+++ 01-Basics/functions.js
@@ -0,0 +1,46 @@
+// // const array=[1,2,3,4,5,6]
+// // for(let i =0;i<array.length;i++){
+// //     console.log(array[i])
+// // }
+
+// const Person =["dhruv","alpit","rahul","jayant"]
+// const CGPA= [7,8,8,7]
+// for (let i =0;i<Person.length;i++){
+//     if(CGPA[i]>7){
+//         console.log(Person[i])
+//     }
+// }
+
+// function Sum(a,b){
+    // return a+b
+// }
+// let a=5,b=7
+// const value =Sum(a,b)
+// console.log(value)
+
+// function sum(num1,num2){
+//     let result = num1+num2
+//     return result
+// }
+// function displayresult(data){
+//     console.log("sum's Result is " +data)
+// }
+// function displayresultPassive(data ,value){
+//     console.log("Sum's Result is " +data)
+// }
+// const ans = sum(1,2)
+// displayresultPassive(100 + displayresult(ans))
+
+// function sum(num1,num2, fntocall){
+//     let result= num1=num2
+//     fntocall(result,displayPassiveresult)
+// }
+// function displayresult(data,fntocall){
+//     console.log("result of the sum is : " +data)
+//     fntocall(data)
+
+// }
+// function displayPassiveresult(data){
+//     console.log("Passiveresult of the sum is : " +data)
+// }
+// const ans = sum(1,2,displayresult)
